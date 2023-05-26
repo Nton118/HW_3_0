473a10c07195de404e580e493d7c2ad6cee0005f
Nton118 <114150459+Nton118@users.noreply.github.com>
2023-05-26 17:47:31 +0300

    HW3_01

---
+++ .history/sort_folder1_20230525191843.py
@@ -0,0 +1,157 @@
+import argparse 
+import json
+from pathlib import Path
+import logging
+import os
+import shutil
+from translit import normalize
+
+
+CATEGORIES = {}
+found_files = {}
+
+known_types = set()
+unknown_types = set()
+deleted_folders = []
+dir_path = os.path.dirname(__file__)
+
+
+def read_config():
+    global CATEGORIES
+
+    with open(os.path.join(dir_path, "config.JSON")) as cfg:
+        cfg_data = json.load(cfg)
+        CATEGORIES = cfg_data["FILETYPES"]
+        for key in CATEGORIES:
+            found_files.update({key: []})
+
+
+
+def scan_folder(path: Path):
+    contents = [x for x in path.iterdir()]
+    for item in contents:
+        is_unknown = True
+        if item.is_file():
+            ext = item.suffix[1:].upper()
+
+            for name, types in CATEGORIES.items():
+                if ext in types:
+                    found_files[name].append(item)
+                    known_types.add(ext)
+                    is_unknown = False
+                    break
+
+            if is_unknown:
+                unknown_types.add(ext)
+
+        else:
+            if item.name not in CATEGORIES.keys():
+                scan_folder(item)
+            else:
+                continue
+
+
+def move_files(files_list: list, target_path: Path, new_folder_name: str) -> list:
+    output_list = []
+    new_dir = target_path / new_folder_name
+    new_dir.mkdir(parents=True, exist_ok=True)
+    for file in files_list:
+        output_list.append(file.name)
+        try:
+            shutil.copyfile(file, new_dir / file.name)
+        except OSError as err:
+            logging.error(err)
+    return output_list
+
+
+def unpack_files(target_path: Path):
+    arc_dir = target_path / "archives"
+    files = [x for x in arc_dir.iterdir()]
+
+    for file in files:
+        new_dir = arc_dir / file.name
+        try:
+            new_dir.mkdir(parents=True, exist_ok=True)
+            shutil.unpack_archive(file, new_dir)
+        except (FileExistsError, shutil.ReadError):
+            pass
+        try:
+            file.unlink()  # delete unpacked archive
+        except FileNotFoundError:
+            pass
+
+
+def normalize_all(path: Path):
+    items = [x for x in path.iterdir()]
+    for item in items:
+        if not item.is_file():
+            if item.name not in CATEGORIES.keys():
+                normalize_all(item)
+                new_name = item.parent / normalize(item.name)
+                item.rename(new_name)
+            else:
+                continue
+        else:
+            new_name = item.parent / normalize(item.name)
+            item.rename(new_name)
+
+
+def report_category(category: str, files_lst: list):
+    return(f'Found files in category "{category.capitalize()}": ', len(files_lst))
+
+
+if __name__ == "__main__":
+    
+    read_config()
+
+    parser = argparse.ArgumentParser(description="Sorting folder")
+    parser.add_argument("--source", "-s", help="Source folder", required=True)
+    parser.add_argument("--output", "-o", help="Output folder", default="dist")
+    parser.add_argument("--translit", "-t", help="Translit names", default=False)
+   
+    
+    print(parser.parse_args())
+    args = vars(parser.parse_args())
+    print(args)
+    transl = args.get("translit")    
+    work_path = Path(args.get("source"))
+    out_path = Path(args.get("output"))
+
+
+    if not work_path:
+        print("Please specify the target path in the parameters")
+    
+    if not work_path.exists():
+        print("The specified path does not exist")
+    
+    try:
+
+        scan_folder(work_path)
+
+        threads = []
+        # create folders only for found file types
+        for category in found_files.keys():
+            if len(found_files.get(category)) > 0:
+                th = Thread()
+                report_category(
+                    category, move_files(found_files.get(category), out_path, str(category))
+                )
+
+        # unpack archives if found any
+        if len(found_files.get("archives")) > 0:
+            unpack_files(out_path)
+            
+        if transl:
+            normalize_all(out_path)
+
+    except PermissionError:
+        print("Change is not allowed. Close the target folder or its subfolders in all other applications.")
+
+    print(f"""Found files of known types: {', '.join(f for f in known_types)}. Total {len(known_types)} files.
+Found files of unknown types: {', '.join(f for f in unknown_types)}. total {len(unknown_types)} files.""")
+    
+    
+
+    
+
+    
---
+++ .history/sort_folder1_20230525232049.py
@@ -0,0 +1,174 @@
+import argparse 
+import json
+import logging
+import os
+import shutil
+from translit import normalize
+from threading import Thread, Condition
+from pathlib import Path
+
+CATEGORIES = {}
+found_files = {}
+
+known_types = set()
+unknown_types = set()
+deleted_folders = []
+dir_path = os.path.dirname(__file__)
+
+
+def read_config():
+    global CATEGORIES
+
+    with open(os.path.join(dir_path, "config.JSON")) as cfg:
+        cfg_data = json.load(cfg)
+        CATEGORIES = cfg_data["FILETYPES"]
+        for key in CATEGORIES:
+            found_files.update({key: []})
+
+
+def th_scan_folder(path: Path):
+    scan_folder(path)
+    with condition:
+        logging.debug('Notify all')
+        condition.notify_all()
+
+def scan_folder(path: Path):
+    contents = [x for x in path.iterdir()]
+    for item in contents:
+        is_unknown = True
+        if item.is_file():
+            ext = item.suffix[1:].upper()
+
+            for name, types in CATEGORIES.items():
+                if ext in types:
+                    found_files[name].append(item)
+                    known_types.add(ext)
+                    is_unknown = False
+                    break
+
+            if is_unknown:
+                unknown_types.add(ext)
+
+        else:
+            if item.name not in CATEGORIES.keys():
+                scan_folder(item)
+            else:
+                continue
+
+
+
+def move_files(files_list: list, target_path: Path, new_folder_name: str) -> list:
+    output_list = []
+    new_dir = target_path / new_folder_name
+    new_dir.mkdir(parents=True, exist_ok=True)
+    for file in files_list:
+        output_list.append(file.name)
+        try:
+            shutil.copyfile(file, new_dir / file.name)
+        except OSError as err:
+            logging.error(err)
+    return output_list
+
+
+def unpack_files(target_path: Path):
+    arc_dir = target_path / "archives"
+    files = [x for x in arc_dir.iterdir()]
+
+    for file in files:
+        new_dir = arc_dir / file.name
+        try:
+            new_dir.mkdir(parents=True, exist_ok=True)
+            shutil.unpack_archive(file, new_dir)
+        except (FileExistsError, shutil.ReadError):
+            pass
+        try:
+            file.unlink()  # delete unpacked archive
+        except FileNotFoundError:
+            pass
+
+
+def normalize_all(path: Path):
+    items = [x for x in path.iterdir()]
+    for item in items:
+        if not item.is_file():
+            if item.name not in CATEGORIES.keys():
+                normalize_all(item)
+                new_name = item.parent / normalize(item.name)
+                item.rename(new_name)
+            else:
+                continue
+        else:
+            new_name = item.parent / normalize(item.name)
+            item.rename(new_name)
+
+
+def report_category(category: str, files_lst: list, condition: Condition):
+    with condition:
+        condition.wait()
+        logging.debug(f'started thread')
+    return(f'Found files in category "{category.capitalize()}": ', len(files_lst))
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG, format="%(threadName)s %(message)s")
+    read_config()
+
+    parser = argparse.ArgumentParser(description="Sorting folder")
+    parser.add_argument("--source", "-s", help="Source folder", default="E:\\Unsorted")
+    parser.add_argument("--output", "-o", help="Output folder", default="dist")
+    parser.add_argument("--translit", "-t", help="Translit names", default=False)
+   
+    
+    print(parser.parse_args())
+    args = vars(parser.parse_args())
+    print(args)
+    transl = args.get("translit")    
+    work_path = Path(args.get("source"))
+    out_path = Path(args.get("output"))
+
+
+    if not work_path:
+        print("Please specify the target path in the parameters")
+    
+    if not work_path.exists():
+        print("The specified path does not exist")
+    
+    try:
+        condition = Condition()
+        threads = []
+        th1=Thread(target=th_scan_folder, args=(work_path, condition,))
+        th1.start()
+        threads.append(th1)
+        
+        
+        # create folders only for found file types
+        for category in found_files.keys():
+            if len(found_files.get(category)) > 0:
+                th = Thread(target=report_category, args=(category, 
+                                                          move_files(found_files.get(category),
+                                                                     out_path,
+                                                                     str(category)),condition,)
+                )
+                th.start()
+                threads.append(th)
+        
+        [th.join() for th in threads]
+                
+        # unpack archives if found any
+        if len(found_files.get("archives")) > 0:
+            unpack_files(out_path)
+            
+        if transl:
+            normalize_all(out_path)
+
+    except PermissionError:
+        print("Change is not allowed. Close the target folder or its subfolders in all other applications.")
+
+    print(f"""Found files of known types: {', '.join(f for f in known_types)}. Total {len(known_types)} files.
+Found files of unknown types: {', '.join(f for f in unknown_types)}. total {len(unknown_types)} files.""")
+    
+    
+
+    
+
+    
